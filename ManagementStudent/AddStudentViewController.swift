46b417cec4cc5121830c2e5b2414fc480ed323d0
Thiện Huỳnh <thienhuynh@Thiens-Mac.local>
2017-05-10 11:45:38 +0700

    move content under keyboard

--- ManagementStudent/AddStudentViewController.swift
+++ ManagementStudent/AddStudentViewController.swift
@@ -8,21 +8,28 @@
 
 import UIKit
 
-class AddStudentViewController: UIViewController, UITextFieldDelegate {
+class AddStudentViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var universityTextField: UITextField!
     @IBOutlet weak var yearOldTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
+    var isActiveField: UITextField?
+    var isActiveTextView: UITextView?
     var studentAdapter: StudentAdapter?
     override func viewDidLoad() {
         super.viewDidLoad()
         self.nameTextField.delegate = self
         self.yearOldTextField.delegate = self
         self.universityTextField.delegate = self
+        self.descriptionTextView.delegate = self
         yearOldTextField.keyboardType = UIKeyboardType.numberPad
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
+        
     }
     
     func setAdapter(studentAdapter: StudentAdapter) {
@@ -59,5 +66,61 @@ class AddStudentViewController: UIViewController, UITextFieldDelegate {
             universityTextField.text = ""
             yearOldTextField.text = ""
         }
+    }
+    
+    // move content under keyboard
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.isActiveField = nil
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.isActiveField = textField
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        self.isActiveTextView = nil
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        self.isActiveTextView = textView
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        print(notification.name)
+        if isActiveField != nil {
+            if let activeField = self.isActiveField, let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+                let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+                
+                self.scrollView.contentInset = contentInsets
+                self.scrollView.scrollIndicatorInsets = contentInsets
+                var aRect = self.view.frame
+                aRect.origin.y += -64
+                aRect.size.height -= keyboardSize.size.height
+                if (!aRect.contains(activeField.frame.origin)) {
+                    self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+                }
+            }
+        }
+        if isActiveTextView != nil {
+            if let activeTextView = self.isActiveTextView, let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+                let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+                
+                self.scrollView.contentInset = contentInsets
+                self.scrollView.scrollIndicatorInsets = contentInsets
+                var aRect = self.view.frame
+                aRect.origin.y += -64
+                aRect.size.height -= keyboardSize.size.height
+                if (!aRect.contains(activeTextView.frame.origin)) {
+                    self.scrollView.scrollRectToVisible(activeTextView.frame, animated: true)
+                }
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
     }
 }
--- ManagementStudent/DetailStudentViewController.swift
+++ ManagementStudent/DetailStudentViewController.swift
@@ -8,26 +8,36 @@
 
 import UIKit
 
-class DetailStudentViewController: UIViewController, UITextFieldDelegate {
+class DetailStudentViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var universityTextField: UITextField!
     @IBOutlet weak var yearOldTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
-
+    var isActiveField: UITextField?
+    var isActiveTextView: UITextView?
     var student: Student?
     override func viewDidLoad() {
         super.viewDidLoad()
         self.nameTextField.delegate = self
         self.yearOldTextField.delegate = self
         self.universityTextField.delegate = self
+        self.descriptionTextView.delegate = self
         yearOldTextField.keyboardType = UIKeyboardType.numberPad
+        
         didSet()
         
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
+        
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardDidHide, object: nil)
+    }
     func setAdapter(student: Student) {
         self.student = student
     }
@@ -57,7 +67,7 @@ class DetailStudentViewController: UIViewController, UITextFieldDelegate {
     // Dismiss the keyboard when the user taps the "Return" key or its equivalent
     // while editing a text field.
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
+        textField.resignFirstResponder()
         return false
     }
     
@@ -65,4 +75,59 @@ class DetailStudentViewController: UIViewController, UITextFieldDelegate {
         view.endEditing(true)
     }
     
+    // move content under keyboard
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.isActiveField = nil
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.isActiveField = textField
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        self.isActiveTextView = nil
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        self.isActiveTextView = textView
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        print(notification.name)
+        if isActiveField != nil {
+            if let activeField = self.isActiveField, let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+                let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+                
+                self.scrollView.contentInset = contentInsets
+                self.scrollView.scrollIndicatorInsets = contentInsets
+                var aRect = self.view.frame
+                aRect.origin.y += -64
+                aRect.size.height -= keyboardSize.size.height
+                if (!aRect.contains(activeField.frame.origin)) {
+                    self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+                }
+            }
+        }
+        if isActiveTextView != nil {
+            if let activeTextView = self.isActiveTextView, let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+                let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+                
+                self.scrollView.contentInset = contentInsets
+                self.scrollView.scrollIndicatorInsets = contentInsets
+                var aRect = self.view.frame
+                aRect.origin.y += -64
+                aRect.size.height -= keyboardSize.size.height
+                if (!aRect.contains(activeTextView.frame.origin)) {
+                    self.scrollView.scrollRectToVisible(activeTextView.frame, animated: true)
+                }
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+    }
 }
